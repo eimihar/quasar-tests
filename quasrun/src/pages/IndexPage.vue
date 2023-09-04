cc61888e86004fca74876db328a5dfd1cb43bfcc
Rahimie <newrehmi@gmail.com>
2023-09-05 02:03:38 +0800

    include setup boot file

--- quasrun/src/pages/IndexPage.vue
+++ quasrun/src/pages/IndexPage.vue
@@ -14,9 +14,13 @@ import { defineComponent } from 'vue';
 import * as process from 'process'
 
 export default defineComponent({
+  mounted() {
+    // not showing when this is tested through quasar dev (spa) or npm run dev
+    console.log(process.env.DEV);
+  },
   computed: {
     mode() {
-      return !!process ? process.env.MODE : '-'
+      return process.env.MODE;
     },
     bootMode() {
       return this.$envMode;
